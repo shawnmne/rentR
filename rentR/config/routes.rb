527d78e116e0a2e92d0433b363e050c147ec7967
Shawn Michaud <shawnmne@hotmail.com>
2016-05-11 11:26:34 -0500

    initial controller for users built

--- rentR/config/routes.rb
+++ rentR/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
 
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/index'
+
+  get 'users/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- rentR/db/migrate/20160510162625_add_rental_id_to_leases.rb
+++
@@ -1,5 +0,0 @@
-class AddRentalIdToLeases < ActiveRecord::Migration
-  def change
-    add_column :leases, :rental_id, :integer
-  end
-end
