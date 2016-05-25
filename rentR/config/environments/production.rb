592f848ed16cdfe5556268d31c7132a88768c2a7
Shawn Michaud <shawnmne@hotmail.com>
2016-05-25 16:22:45 -0500

    removed production configuratin for mailer

--- rentR/config/environments/production.rb
+++ rentR/config/environments/production.rb
@@ -16,15 +16,8 @@ Rails.application.configure do
   config.cache_classes = true
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.sendgrid.net',
-  port:                 25,
-  domain:               ENV["DOMAIN"],
-  user_name:            ENV["EMAIL_ADDRESS"],
-  password:             ENV["PASSWORD"],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+ 
+ 
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
